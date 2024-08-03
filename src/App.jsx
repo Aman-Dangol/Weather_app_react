382bc0395a6d235dda47ad6dc8e151c3b47aaea3
Aman Dangol <amandangol423@gmail.com>
2024-08-03 21:26:48 +0545

    styled main container

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { apiKey } from "./apiKey";
 import "./css_modules/global.css";
+import MainContainer from "./components/mainContainer";
 import Header from "./components/header";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <MainContainer></MainContainer>
     </>
   );
 }
---
+++ src/components/mainContainer.jsx
@@ -0,0 +1,5 @@
+import styles from "../css_modules/mainContainer.module.css";
+
+export default function MainContainer() {
+  return <div className={styles.container}>its a main container!!!</div>;
+}
