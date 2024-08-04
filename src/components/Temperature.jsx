f3582cbaea975cb785850b8ecfea0fb89a26ee97
Aman Dangol <amandangol423@gmail.com>
2024-08-04 20:47:18 +0545

    temperature styling

--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -3,11 +3,11 @@ export default function Temperature({ weatherData }) {
   return (
     <div className={styles.container}>
       <h2>
-        {weatherData.main.temp} <sup>o</sup>C
+        {weatherData.main.temp}<sup>o</sup>C
       </h2>
       <p>
         feels like : <strong>{weatherData.main["feels_like"]}</strong>
-        <sup>o</sup> C{" "}
+        <sup>o</sup>C
       </p>
     </div>
   );
--- src/components/WeatherContent.jsx
+++ src/components/WeatherContent.jsx
@@ -1,4 +1,11 @@
 import styles from "../css_modules/weatherContent.module.css";
 export default function WeatherContent({ weatherData }) {
-  return <div className={styles.container}>{weatherData.coord.lat}</div>
+  return (
+    <div className={styles.container}>
+      <img
+        src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+        alt="image"
+      />
+    </div>
+  );
 }
