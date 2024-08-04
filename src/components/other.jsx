ee5210be0dbb1896a5ef2684836f5a08b0e018f2
Aman Dangol <amandangol423@gmail.com>
2024-08-04 22:02:09 +0545

    third div work

---
+++ src/components/other.jsx
@@ -0,0 +1,12 @@
+import styles from "../css_modules/other.module.css";
+
+export default function Other() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.ddiv}>d</div>
+      <div className={styles.ddiv}>d</div>
+      <div className={styles.ddiv}>d</div>
+      <div className={styles.ddiv}>d</div>
+    </div>
+  );
+}
