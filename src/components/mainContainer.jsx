3d53da48a182136baa3217686e5d0aa8ac776c9d
Aman Dangol <amandangol423@gmail.com>
2024-08-04 22:01:50 +0545

    dividing divs

--- src/components/mainContainer.jsx
+++ src/components/mainContainer.jsx
@@ -1,6 +1,7 @@
 import styles from "../css_modules/mainContainer.module.css";
 import WeatherContent from "./WeatherContent";
 import Temperature from "./Temperature";
+import Other from "./other";
 export default function MainContainer({ weatherData }) {
   let ele = <div>loading</div>;
   if (weatherData) {
@@ -8,7 +9,7 @@ export default function MainContainer({ weatherData }) {
       <div className={styles.container}>
         <Temperature weatherData={weatherData}></Temperature>
         <WeatherContent weatherData={weatherData}></WeatherContent>
-        <div style={{ backgroundColor: "red", textAlign: "center" }}>div 3</div>
+        <Other></Other>
       </div>
     );
   }
